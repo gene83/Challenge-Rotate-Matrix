34b054e93c7df02edf86fd829eea926cc63cc4d5
Gene83 <gene83@protonmail.com>
2019-01-16 10:44:09 -1000

    find transpose then reverse rows for clockwise rotation or reverse columns for counter clockwise direction

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -1,4 +1,4 @@
-const Direction = require("./Direction").Direction;
+const Direction = require('./Direction').Direction;
 
 /*  MatrixRotator(matrix)
  *
@@ -11,16 +11,40 @@ const Direction = require("./Direction").Direction;
  *        @returns the rotated matrix
  */
 module.exports = class MatrixRotator {
-  constructor( matrix ) {
+  constructor(matrix) {
     this.matrix = matrix;
   }
 
   //      |-- Must be Direction.CW
   //      v        or Direction.CCW
-  rotate (direction) {
-    // do work here
+  rotate(direction) {
+    const width = this.matrix[0].length;
+    const height = this.matrix.length;
+    const transpose = [];
 
-    // must be a valid Direction, see Direction.js
+    for (let y = 0; y < height; y++) {
+      transpose[y] = [];
+      for (let x = 0; x < width; x++) {
+        transpose[y][x] = this.matrix[x][y];
+      }
+    }
+    if (direction == 'ClockWise') {
+      transpose.forEach(row => row.reverse());
+      this.matrix = transpose;
+    } else {
+      const newHeight = width;
+      const newWidth = height;
 
+      const columnsReversed = [];
+
+      for (let y = 0; y < newHeight; y++) {
+        columnsReversed[y] = [];
+        for (let x = 0; x < newWidth; x++) {
+          columnsReversed[y][x] = transpose[newHeight - y - 1][x];
+        }
+      }
+
+      this.matrix = columnsReversed;
+    }
   }
 };
